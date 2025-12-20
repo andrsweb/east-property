1e8be4b2d17fc93da8193f76b1d0ed1635b5031e
andrsweb <d3vv.proo@gmail.com>
2025-12-20 21:44:32 +0700

    feat: create animated contact panel

--- dev/src/js/main.js
+++ dev/src/js/main.js
@@ -1,10 +1,10 @@
 import './common/common';
 import './common/tabs';
-// Components
+// Components && Sections js
 import '../html/components/ui/search-tabs/search-tabs';
 import '../html/components/common/header/header-menu';
+import '../html/components/ui/contact-panel/contact-panel';
 import '../html/sections/index/map/map';
 import '../html/components/filters/result-filters/filters';
-// Single page sliders
-import '../html/sections/single/items/swiper.js';
+import '../html/sections/single/items/swiper';
 import '../html/sections/single/dropdowns/dropdowns';
